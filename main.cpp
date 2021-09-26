6c57277e4b2069f500299d9e34ac53b31ed6c0aa
Fred <wictorsson@gmail.com>
2021-09-26 20:25:59 +0000

    proj 5 part1

--- main.cpp
+++ main.cpp
@@ -35,29 +35,222 @@ Purpose:  This project continues developing Project3.
  8) After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
  
  */
-
+#include <iostream>
 /*
  copied UDT 1:
  */
+struct Guitar
+{
+    Guitar();
+    ~Guitar();
+    
+    int numberOfPickups = 2;
+    int numberOfStrings = 6;
+    std::string logoName;
+    std::string color;
+    bool tuningSystem;
+    
+    struct String
+    {
+        String();
+        ~String();
+        
+        std::string winding = "flatwound";
+        std::string manufactor = "Earnie ball";
+        float length;
+        std::string material;
+        int age;
+    };
+};
 
+Guitar::Guitar() : logoName("Gibson"), color("Black"), tuningSystem(true) 
+{
+    std::cout << "construct Guitar" << std::endl;
+} 
+
+Guitar::~Guitar()
+{
+    std::cout << "deconstruct Guitar" << std::endl; 
+}
+
+Guitar::String::String() : length (100.0f), age (12) 
+{
+    std::cout << "construct string" << std::endl;
+} 
+
+Guitar::String::~String()
+{
+    std::cout << "deconstruct String" << std::endl; 
+}
 /*
  copied UDT 2:
  */
+struct TVStation
+{
+    TVStation();
+    ~TVStation();
+    int channelChains = 24;
+    int numberOfEmployees = 202;
+    int numberOfCameras = 12;
+    std::string tvStationName;
+    float satelliteRange;
 
+    struct Studio
+    {
+        Studio();
+        ~Studio();
+        bool isNewsStudio = false;
+        int lightbulbs = 1000;
+        bool isOnAir;
+        int cameras;
+        float size;
+
+        void distributeLamps(int lampsPerRoom);
+        void transmitVideo(bool cameraIsOn);
+        void transmitAudio(bool microphone);
+        bool getOnAirStatus(bool isCameraOn);
+    };
+
+    void changeSatelliteRange(float steps, bool wider);
+    void broadcastChannel(std::string channel);
+    void produceVideo(Studio studio);
+    std::string getFeedback(int date, std::string feedBack);
+};
+
+TVStation::TVStation() : tvStationName("Nickelodeon"), satelliteRange(180.0f) {
+    std::cout << "construct TV station" << std::endl;
+} 
+
+TVStation::~TVStation() 
+{
+std::cout << "deconstruct TVStation" << std::endl;
+}
+
+TVStation::Studio::Studio() : isOnAir(false), cameras(5), size(100) 
+{
+    std::cout << "construct studio" << std::endl;
+} 
+
+TVStation::Studio::~Studio()
+{
+    std::cout << "deconstruct Studio" << std::endl;
+}
 /*
  copied UDT 3:
  */
+struct VendingMashine
+{
+    VendingMashine();
+    ~VendingMashine();
+    
+    int amountOfCandy = 50;
+    int amountOfDrinks = 50;
+    float insertedCoins;
+    bool coolingSystem;
+    std::string itemTag;
 
+    float purchaseWithFiftyCents(float coinValue,int itemPrice);
+    float chargeCustomer(bool creditcard, float itemPrice);
+    void feedCustomer(int amountOfCandy);
+    void coolDownMashine(int temperature, int duration);
+};
+
+VendingMashine::VendingMashine() : insertedCoins(10), coolingSystem(true), itemTag("Orego")
+{
+    std::cout << "construct vendingmashine" << std::endl;
+} 
+
+VendingMashine::~VendingMashine()
+{
+    std::cout << "deconstruct VendingMashine" << std::endl;
+}
 /*
  new UDT 4:
  with 2 member functions
  */
+struct Shop
+{
+    Shop();
+    ~Shop();
+    Guitar guitar;
+    Guitar::String string;
+    TVStation tvstation;
+    VendingMashine vendingMashine;
 
+    void sellParts(int noOfItems);
+    int repairedItem(std::string itemName);
+};
+
+Shop::Shop()
+ {
+     std::cout << "Construct shop, guitar pickups available: " << guitar.numberOfPickups << std::endl;
+ }
+ 
+Shop::~Shop()
+{
+    std::cout << "deconstruct shop, guitar pickups available: " << guitar.numberOfPickups << std::endl;
+}
+
+void Shop::sellParts(int noOfItems)
+{
+    guitar.numberOfPickups -= noOfItems;
+}
+
+int Shop::repairedItem(std::string itemName)
+{
+    if (vendingMashine.coolingSystem)
+    {
+        std::cout << itemName << " has been repaired" << std::endl;
+        return 1;
+    }
+    return 0;
+}
 /*
  new UDT 5:
  with 2 member functions
  */
+struct MediaHouse
+{   
+    MediaHouse();
+    ~MediaHouse();
+    TVStation tvstation;
+    VendingMashine vendingMashine;
 
+    void cleanArea();
+    void shutDownItem(std::string item);
+};
+
+MediaHouse::MediaHouse()
+{
+    std::cout << "Construct MediaHouse" << std::endl;
+}
+
+MediaHouse::~MediaHouse()
+{
+    std::cout << "deconstruct MediaHouse" << std::endl;
+}
+
+void MediaHouse::cleanArea()
+{
+    std::cout << "Cleaning TV station: " << tvstation.tvStationName << std::endl;
+}
+
+void MediaHouse::shutDownItem(std::string item)
+{
+    if (item == "Vendingmashine")
+    {
+        std::cout << "shutting down vendingmashine" << std::endl;
+        vendingMashine.amountOfCandy = 0;
+        vendingMashine.amountOfDrinks = 0;
+    } 
+    else if (item == "TVstation")
+    {
+        std::cout << "shutting down TV station" << std::endl;
+        tvstation.numberOfEmployees=0;
+    }
+    
+     
+}
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
 
@@ -72,8 +265,29 @@ Purpose:  This project continues developing Project3.
  Wait for my code review.
  */
 
-#include <iostream>
+//#include <iostream>
 int main()
 {
+    Guitar guitar;
+    Guitar::String string;
+    std::cout << std::endl;
+
+    TVStation tvStation;
+    TVStation::Studio studio;
+    std::cout << std::endl;
+
+    VendingMashine vMachine;
+    std::cout << std::endl;
+
+    Shop shop;
+    shop.sellParts(2);
+    shop.repairedItem("Cooling system");
+    std::cout << std::endl;
+
+    MediaHouse mediahouse;
+    mediahouse.cleanArea();
+    mediahouse.shutDownItem("Vendingmashine");
+    std::cout << std::endl;
+
     std::cout << "good to go!" << std::endl;
 }
